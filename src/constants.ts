024a4ab0d92804b9c0df43aa0447fca2b89e1dd6
James Abney <james.abney@gmail.com>
2019-06-01 11:24:57 -0700

    Remove files

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-import path from 'path'
-
-export const FALLBACK_PATH = path.resolve(__dirname, '../words.txt')
--- src/list-info.ts
+++
@@ -1,5 +0,0 @@
-
-export interface IListInfo {
-  isFallback: boolean
-  list: string[]
-}
