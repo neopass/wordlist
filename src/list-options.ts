8c13e554aea0980c90d7f7174e74e336dd70e914
James Abney <james.abney@gmail.com>
2019-06-01 12:53:33 -0700

    Add 'combine' option

--- src/list-options.ts
+++ src/list-options.ts
@@ -2,13 +2,18 @@
 export interface IListOptions {
   /**
    * Word list paths to search for in order. Only the first
-   * one found is used.
+   * one found is used. This option is ignored if 'combine'
+   * is a non-empty array.
    *
    * default: [
    *  '/usr/share/dict/words'
    * ]
    */
   paths?: string[]
+  /**
+   * Word list paths to combine. All found files are used.
+   */
+  combine?: string[]
 }
 
 /**
