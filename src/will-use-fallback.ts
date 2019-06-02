fa61f8ca970fd8681656e17fb2d29496fee70cbf
James Abney <james.abney@gmail.com>
2019-06-02 13:26:27 -0700

    Implement willUseFallback function

---
+++ src/will-use-fallback.ts
@@ -0,0 +1,17 @@
+import { IListOptions, defaultOptions } from './list-options'
+import { pathAlias } from './path-alias'
+import { isFile } from './is-file'
+
+/**
+ * Determine if the fallback dictionary will be used
+ * given the provided options.
+ */
+export function willUseFallback(options: IListOptions) {
+  const opts = {...defaultOptions, ...options}
+  const paths = opts.paths || []
+  const path = paths
+    .map(path => pathAlias(path))
+    .filter(path => isFile(path))[0]
+
+  return path === pathAlias('$fallback')
+}
