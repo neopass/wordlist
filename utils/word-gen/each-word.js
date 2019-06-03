4bacf5f223d35deef1bfd74993a4f6f6d4bcaf2b
James Abney <james.abney@gmail.com>
2019-06-02 22:10:18 -0700

    Use word splitting logic to process lines

--- utils/word-gen/each-word.js
+++ utils/word-gen/each-word.js
@@ -1,25 +1,42 @@
 'use strict'
 
-const { regexEach } = require('regex-each')
-
 /**
  * @typedef {(word: string) => void} WordCbk
  */
 
-const dia = 'àáâäæãåāçćčèéêëēėęîïíīįìłñńôöòóœøōõßśšûüùúūÿžźż'
-const other = '’'
-const reWord = new RegExp(`[a-z-'${dia}${other}]+`, 'gi')
+// Zero-width space.
+const zSpace = '\u200b'
+// Zero-width joiner.
+const zJoin = '\u200d'
+// Ascii 151 (0x97)
+const z151 = '\u0097'
+
+// Figure dash.
+const figDash = '\u2012'
+// Em dash.
+const emDash = '\u2013'
+// En dash.
+const enDash = '\u2014'
+// Horizontal bar.
+const hBar = '\u2015'
+// Swung dash.
+const sDash = '\u2053'
+// Hyphen.
+const hyphen = '\u2010'
+
+const dashes = `${figDash}${emDash}${enDash}${hBar}${sDash}${hyphen}`
+const zero = `${zSpace}${zJoin}${z151}`
+
+// Split on spaces and punctuation.
+const reSplitter = new RegExp(String.raw`[\[\]\s/.?!,:;(){}~*_=│|…${dashes}-]+`)
+
+const reZeroes = new RegExp(`[${zero}]`, 'g')
 
 /**
- * @param {string} word
- *
- * @returns {string}
+ * @todo Move `'s` removal to a post-word transform step.
  */
-function transform(word) {
-  return word
-    // Single quote.
-    .replace(/[’]/, '\'')
-}
+const qMarks = `'’‘"“”`
+const reRemove = new RegExp(`^[${qMarks}]+|[${qMarks}]+$|[${qMarks}]s$`, 'gi')
 
 /**
  * @param {string} line
@@ -28,9 +45,10 @@ function transform(word) {
  * @returns {void}
  */
 function eachWord(line, onWord) {
-  regexEach(reWord, line, (match) => {
-    onWord(transform(match[0]))
-  })
+  let words = line.split(reSplitter)
+  words = words.map((word) => word.replace(reZeroes, '').replace(reRemove, ''))
+  words.forEach((word) => { if (word.length > 0) { onWord(word) } })
+  // words.forEach(word => { if (word.startsWith('"')) { console.log(word) }})
 }
 
 module.exports = eachWord
