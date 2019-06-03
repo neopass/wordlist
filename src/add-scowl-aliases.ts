316b29470a016546900e6356a0dd4796cc34716b
James Abney <james.abney@gmail.com>
2019-06-02 21:59:34 -0700

    Implement addScowlAliases

---
+++ src/add-scowl-aliases.ts
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+
+export function addScowlAliases(map: Map<string, string>) {
+  const scowlPath = path.resolve(__dirname, '../scowl/words')
+
+  if (!fs.existsSync(scowlPath)) {
+    return console.error('could not find scowl words at ' + scowlPath)
+  }
+
+  const names = fs.readdirSync(scowlPath)
+  names.forEach(name => map.set(`$${name}`, path.resolve(__dirname, `../scowl/words/${name}`)))
+  console.log(map)
+}
