6b20fb9ad3dbb0ab4c36756f36ee36730156d34a
James Abney <james.abney@gmail.com>
2019-06-02 22:08:38 -0700

    Add scowl aliases to map

--- src/path-alias.ts
+++ src/path-alias.ts
@@ -1,8 +1,11 @@
 import path from 'path'
+import { addScowlAliases } from './add-scowl-aliases'
 
 const aliasMap = new Map([
-  ['$fallback', path.resolve(__dirname, '../fallback.txt')]
+  ['$fallback', path.resolve(__dirname, '../fallback.txt')],
 ])
+
+addScowlAliases(aliasMap)
 
 /**
  * Return a path alias if it exists, otherwise return the given path.
