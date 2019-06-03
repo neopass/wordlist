7476b21f185a52c652f60dd61dea676381f2a0d2
James Abney <james.abney@gmail.com>
2019-06-03 01:07:52 -0700

    Add scowl alias helper

--- src/path-alias.ts
+++ src/path-alias.ts
@@ -1,10 +1,27 @@
+import fs from 'fs'
 import path from 'path'
-import { addScowlAliases } from './add-scowl-aliases'
+// import { addScowlAliases } from './add-scowl-aliases'
 
+/**
+ * Add scowl source aliases to the map.
+ */
+function addScowlAliases(map: Map<string, string>) {
+  const scowlPath = path.resolve(__dirname, '../scowl/words')
+
+  if (!fs.existsSync(scowlPath)) {
+    return console.error('could not find scowl words at ' + scowlPath)
+  }
+
+  const names = fs.readdirSync(scowlPath)
+  names.forEach(name => map.set(`$${name}`, path.resolve(__dirname, `../scowl/words/${name}`)))
+}
+
+// Create a map of { alias => path }.
 const aliasMap = new Map([
-  ['$default', path.resolve(__dirname, '../fallback.txt')],
+  ['$default', path.resolve(__dirname, '../default.txt')],
 ])
 
+// Add scowl aliases.
 addScowlAliases(aliasMap)
 
 /**
