fba5992238a308f717071a26d4a3569f9d24ed7b
James Abney <james.abney@gmail.com>
2019-06-05 09:14:58 -0700

    Use async specs

--- test/wordlist.spec.ts
+++ test/wordlist.spec.ts
@@ -2,28 +2,24 @@ import assert from 'assert'
 import { wordList, wordListSync } from '../src'
 
 describe('wordList', () => {
-  it('works with no configuration', () => {
-    return wordList().then((list) => {
-      assert.strictEqual(list.length, 86748)
-    })
+  it('works with no configuration', async () => {
+    const list = await wordList()
+    assert.strictEqual(list.length, 86748)
   })
 
-  it('allows paths overrides', () => {
-    return wordList({paths: ['$special-hacker.50']}).then((list) => {
-      assert.strictEqual(list.length, 1649)
-    })
+  it('allows paths overrides', async () => {
+    const list = await wordList({paths: ['$special-hacker.50']})
+    assert.strictEqual(list.length, 1649)
   })
 
-  it('falls back when path not found', () => {
-    return wordList({paths: ['/no/file/here', '$special-hacker.50']}).then((list) => {
-      assert.strictEqual(list.length, 1649)
-    })
+  it('falls back when path not found', async () => {
+    const list = await wordList({paths: ['/no/file/here', '$special-hacker.50']})
+    assert.strictEqual(list.length, 1649)
   })
 
-  it('combines files', () => {
-    return wordList({combine: ['$special-hacker.50', '$variant_1-words.95']}).then((list) => {
-      assert.strictEqual(list.length, 2014)
-    })
+  it('combines files', async () => {
+    const list = await wordList({combine: ['$special-hacker.50', '$variant_1-words.95']})
+    assert.strictEqual(list.length, 2014)
   })
 })
 
